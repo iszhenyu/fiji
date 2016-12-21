b4ab9519413279018c611c04079d06f076080baa
zhen.yu <zhen.yu@17zuoye.com>
2016-12-21 16:26:36 +0800

    ..

---
+++ app/listeners/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+@author: xiaoz
+@time: 2016/12/21 下午4:22
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
