ffe284ac0ef701896f03f9e1de8ff239e49be95f
zhen.yu <zhen.yu@17zuoye.com>
2017-02-04 17:01:18 +0800

    ..

--- manage.py
+++ manage.py
@@ -3,16 +3,10 @@
 @author: xiaoz
 @time: 2016/12/21 下午5:50
 """
-import os
-
 from app.controllers import create_app
 
 app = create_app()
 
 
 if __name__ == '__main__':
-    # c = os.path.abspath(__file__)
-    # print c
-    # p = os.path.dirname(c)
-    # print os.path.dirname(p)
     app.run()
